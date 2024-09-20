dd91eecc7aec0209ed159be2c5ae66fa4265d7ae
Noah F <noah.franck@Lafayette-engineering.com>
2024-09-20 15:30:25 -0400

    UPdated with more obsidian plugins & keybindings

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -26,6 +26,23 @@ return {
           return require('obsidian').util.toggle_checkbox()
         end, { desc = '[O]bsidian [C]heckbox [T]oggle' }),
 
+        note_id_func = function(title)
+          -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+          -- In this case a note with the title 'My new note' will be given an ID that looks
+          -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+          local suffix = ''
+          if title ~= nil then
+            -- If title is given, transform it into valid file name.
+            return tostring(title) .. '.md'
+          else
+            -- If title is nil, just add 4 random uppercase letters to the suffix.
+            for _ = 1, 4 do
+              suffix = suffix .. string.char(math.random(65, 90))
+            end
+            return tostring(os.time()) .. '-' .. suffix
+          end
+        end,
+
         ui = {
           enable = true, -- set to false to disable all additional syntax features
           update_debounce = 200, -- update delay after a text change (in milliseconds)
